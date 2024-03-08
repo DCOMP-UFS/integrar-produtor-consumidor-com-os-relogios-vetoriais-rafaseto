813d970ae26ab085d4870fdb20e8a5377fcda91a
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-08 03:18:10 -0300

    implementa duas filas

--- test_01.c
+++ test_01.c
@@ -12,116 +12,120 @@ typedef struct Clock {
     long idProducer;     // Identificador do produtor do relógio
 } Clock;
 
-Clock clockQueue[BUFFER_SIZE];  // Buffer para armazenar os relógios produzidos
-int clockCount = 0;              // Contador para o número de relógios presentes no buffer
+// Buffer para armazenar os relógios entre Receive e Update
+Clock clockQueueReceiveUpdate[BUFFER_SIZE];
+int clockCountReceiveUpdate = 0;
 
-pthread_mutex_t mutex;           // Mutex para garantir exclusão mútua no acesso ao buffer
-pthread_cond_t condFull;         // Variável de condição para indicar que o buffer está cheio
-pthread_cond_t condEmpty;        // Variável de condição para indicar que o buffer está vazio
+// Buffer para armazenar os relógios entre Update e Send
+Clock clockQueueUpdateSend[BUFFER_SIZE];
+int clockCountUpdateSend = 0;
 
-// Função para imprimir o estado do relógio
-void Clock_logging(int pid, Clock *clock){
-    printf("Process: %d, Clock: (%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]);
-}
+pthread_mutex_t mutexReceiveUpdate;
+pthread_cond_t condFullReceiveUpdate;
+pthread_cond_t condEmptyReceiveUpdate;
 
-// Função para simular um evento incrementando o relógio de um processo
-void Event(int pid, Clock *clock, int logg){
-    clock->p[pid]++;  // Incrementa o relógio do processo especificado
-    if (logg) {Clock_logging(pid, clock);}  // Se logg for verdadeiro, imprime o estado do relógio
-}
+pthread_mutex_t mutexUpdateSend;
+pthread_cond_t condFullUpdateSend;
+pthread_cond_t condEmptyUpdateSend;
 
-// Função para enviar um relógio para outro processo via MPI
-void Send(int pid_send_to, Clock *clock, int pid_sender){
-    Event(pid_sender, clock, 0);  // Incrementa o relógio do processo remetente
-    MPI_Send(&clock->p[0], 3, MPI_INT, pid_send_to, 0, MPI_COMM_WORLD);  // Envia o relógio via MPI
-    Clock_logging(pid_sender, clock);  // Imprime o estado do relógio após o envio
-}
+// Função para obter um relógio do buffer entre Receive e Update
+Clock getClockReceiveUpdate(){
+    pthread_mutex_lock(&mutexReceiveUpdate);  
 
-// Função para receber um relógio de outro processo via MPI
-void Receive(int pid_receive_from, Clock *clock, int pid_receiver){
-    Clock temp_clock = {{0,0,0}};  // Cria um relógio temporário com todos os valores inicializados para zero
-    MPI_Status status;
-    Event(pid_receiver, clock, 0);  // Incrementa o relógio do processo receptor
-    MPI_Recv(&temp_clock.p[0], 3, MPI_INT, pid_receive_from, 0, MPI_COMM_WORLD, &status);  // Recebe o relógio via MPI
-    for (int i = 0; i < 3; i++){
-        clock->p[i] = (temp_clock.p[i] > clock->p[i]) ? temp_clock.p[i] : clock->p[i];  // Atualiza o relógio local com os valores recebidos
-    }
-    Clock_logging(pid_receiver, clock);  // Imprime o estado do relógio após a recepção
-}
-
-// Função para consumir (imprimir) um relógio produzido
-void consumeClock(Clock *clock, int idConsumer){
-    printf("(Consumer Thread %d) Consuming clock {%d, %d, %d} produced by  (Producer Thread %ld)\n", (idConsumer + 1)/2, clock->p[0], clock->p[1], clock->p[2], clock->idProducer);
-}
-
-// Função para obter um relógio do buffer
-Clock getClock(){
-    pthread_mutex_lock(&mutex);  // Bloqueia o mutex para garantir exclusão mútua
-
-    while (clockCount == 0){
-        pthread_cond_wait(&condEmpty, &mutex);  // Aguarda até que o buffer não esteja vazio
+    while (clockCountReceiveUpdate == 0){
+        pthread_cond_wait(&condEmptyReceiveUpdate, &mutexReceiveUpdate);
     }
 
-    Clock clock = clockQueue[0];  // Obtém o relógio do buffer
+    Clock clock = clockQueueReceiveUpdate[0];  
     int i;
-    for (i = 0; i < clockCount - 1; i++){
-        clockQueue[i] = clockQueue[i+1];  // Move os relógios restantes no buffer para frente
+    for (i = 0; i < clockCountReceiveUpdate - 1; i++){
+        clockQueueReceiveUpdate[i] = clockQueueReceiveUpdate[i+1];  
     }
-    clockCount--;  // Atualiza o contador de relógios no buffer
+    clockCountReceiveUpdate--;  
 
-    pthread_mutex_unlock(&mutex);  // Libera o mutex
-    pthread_cond_signal(&condFull);  // Sinaliza que o buffer não está mais cheio
+    pthread_mutex_unlock(&mutexReceiveUpdate);
+    pthread_cond_signal(&condFullReceiveUpdate);
 
-    return clock;  // Retorna o relógio obtido
+    return clock;  
 }
 
-// Função para submeter um relógio ao buffer
-void submitClock(Clock clock){
-    pthread_mutex_lock(&mutex);  // Bloqueia o mutex para garantir exclusão mútua
+// Função para submeter um relógio ao buffer entre Receive e Update
+void submitClockReceiveUpdate(Clock clock){
+    pthread_mutex_lock(&mutexReceiveUpdate);
 
-    while (clockCount == BUFFER_SIZE){
-        pthread_cond_wait(&condFull, &mutex);  // Aguarda até que o buffer não esteja cheio
+    while (clockCountReceiveUpdate == BUFFER_SIZE){
+        pthread_cond_wait(&condFullReceiveUpdate, &mutexReceiveUpdate);
     }
 
-    clockQueue[clockCount] = clock;  // Adiciona o relógio ao buffer
-    clockCount++;  // Atualiza o contador de relógios no buffer
+    clockQueueReceiveUpdate[clockCountReceiveUpdate] = clock;
+    clockCountReceiveUpdate++;
 
-    pthread_mutex_unlock(&mutex);  // Libera o mutex
-    pthread_cond_signal(&condEmpty);  // Sinaliza que o buffer não está mais vazio
+    pthread_mutex_unlock(&mutexReceiveUpdate);
+    pthread_cond_signal(&condEmptyReceiveUpdate);
 }
 
-// Função da thread para enviar relógios
-void* startSenderThread(void* arg) {
-    int my_id = *(int*)arg;
-    while (1) {
-        Clock clock = getClock();
-        Event(my_id, &clock, 1);
-        Send(0, &clock, my_id);
-        usleep(1000000);
+// Função para obter um relógio do buffer entre Update e Send
+Clock getClockUpdateSend(){
+    pthread_mutex_lock(&mutexUpdateSend);
+
+    while (clockCountUpdateSend == 0){
+        pthread_cond_wait(&condEmptyUpdateSend, &mutexUpdateSend);
     }
-    return NULL;
+
+    Clock clock = clockQueueUpdateSend[0];
+    int i;
+    for (i = 0; i < clockCountUpdateSend - 1; i++){
+        clockQueueUpdateSend[i] = clockQueueUpdateSend[i+1];
+    }
+    clockCountUpdateSend--;
+
+    pthread_mutex_unlock(&mutexUpdateSend);
+    pthread_cond_signal(&condFullUpdateSend);
+
+    return clock;
 }
 
-// Função da thread para receber relógios
-void* startReceiverThread(void* arg) {
-    int my_id = *(int*)arg;
-    while (1) {
-        Clock clock;
-        Receive(0, &clock, my_id);
-        submitClock(clock);
+// Função para submeter um relógio ao buffer entre Update e Send
+void submitClockUpdateSend(Clock clock){
+    pthread_mutex_lock(&mutexUpdateSend);
+
+    while (clockCountUpdateSend == BUFFER_SIZE){
+        pthread_cond_wait(&condFullUpdateSend, &mutexUpdateSend);
     }
-    return NULL;
+
+    clockQueueUpdateSend[clockCountUpdateSend] = clock;
+    clockCountUpdateSend++;
+
+    pthread_mutex_unlock(&mutexUpdateSend);
+    pthread_cond_signal(&condEmptyUpdateSend);
 }
 
-// Função da thread para atualizar relógios
-void* startUpdaterThread(void* arg) {
-    int my_id = *(int*)arg;
-    while (1) {
-        Clock clock = getClock();
-        Event(my_id, &clock, 1);
-        submitClock(clock);
+// Função para receber um relógio de outro processo via MPI e atualizar o relógio local
+void Receive(int pid_receive_from, Clock *clock, int pid_receiver){
+    Clock temp_clock = {{0,0,0}};  
+    MPI_Status status;
+    MPI_Recv(&temp_clock.p[0], 3, MPI_INT, pid_receive_from, 0, MPI_COMM_WORLD, &status);  
+    for (int i = 0; i < 3; i++){
+        clock->p[i] = (temp_clock.p[i] > clock->p[i]) ? temp_clock.p[i] : clock->p[i];  
     }
-    return NULL;
+    printf("Process %d received clock {%d, %d, %d} from process %d\n", pid_receiver, clock->p[0], clock->p[1], clock->p[2], pid_receive_from);
+    submitClockReceiveUpdate(*clock); // Submete o relógio ao buffer entre Receive e Update
+}
+
+// Função para atualizar o relógio e submetê-lo ao buffer entre Update e Send
+void Update(int pid_updater, Clock *clock, int pid_receiver){
+    Clock updatedClock = getClockReceiveUpdate(); // Obtém um relógio do buffer entre Receive e Update
+    updatedClock.p[pid_updater]++; // Incrementa o relógio do processo atual
+    printf("Process %d updated clock to {%d, %d, %d}\n", pid_updater, updatedClock.p[0], updatedClock.p[1], updatedClock.p[2]);
+    submitClockUpdateSend(updatedClock); // Submete o relógio ao buffer entre Update e Send
+}
+
+// Função para enviar um relógio para outro processo via MPI e consumir o relógio produzido
+void Send(int pid_send_to, Clock *clock, int pid_sender){
+    MPI_Send(&clock->p[0], 3, MPI_INT, pid_send_to, 0, MPI_COMM_WORLD);  
+    printf("Process %d sent clock {%d, %d, %d} to process %d\n", pid_sender, clock->p[0], clock->p[1], clock->p[2], pid_send_to);
+    Clock consumedClock = getClockUpdateSend(); // Obtém um relógio do buffer entre Update e Send para consumo
+    printf("Process %d received clock {%d, %d, %d} back from buffer\n", pid_sender, consumedClock.p[0], consumedClock.p[1], consumedClock.p[2]);
 }
 
 void callProcess(int my_id) {
@@ -129,23 +133,23 @@ void callProcess(int my_id) {
 
     switch (my_id) {
         case 0:
-            Event(my_id, &clock, 1);
+            Update(my_id, &clock, 1);
             Send(1, &clock, my_id);
             Receive(1, &clock, my_id);
             Send(2, &clock, my_id);
             Receive(2, &clock, my_id);
             Send(1, &clock, my_id);
-            Event(my_id, &clock, 1);
+            Update(my_id, &clock, 1);
             break;
 
         case 1:
             Send(0, &clock, my_id);
             Receive(0, &clock, my_id);
-            Receive(0, &clock, my_id);
+            Update(my_id, &clock, 1); 
             break;
 
         case 2:
-            Event(my_id, &clock, 1);
+            Update(my_id, &clock, 1);
             Send(0, &clock, my_id);
             Receive(0, &clock, my_id);
             break;
